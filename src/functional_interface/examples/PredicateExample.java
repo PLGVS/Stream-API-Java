561fcb4de92ad7a3529c3cf5d8a50b89a3f97338
PLGVS <pedro.gaspar1214@gmail.com>
2025-03-07 16:09:00 -0300

    Adicionando exemplos de Functional Interfaces
    Signed-off-by: PLGVS <pedro.gaspar1214@gmail.com>

---
+++ src/functional_interface/examples/PredicateExample.java
@@ -0,0 +1,17 @@
+package functional_interface.examples;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class PredicateExample {
+    public static void main(String[] args) {
+        List<String> palavras = Arrays.asList("java", "kotlin", "python", "javascript", "c", "go", "ruby");
+
+        Predicate<String> maisDeCincoCaracteres = palavra -> palavra.length() > 5;
+
+        palavras.stream()
+                .filter(p -> p.length() > 5)
+                .forEach(palavra -> System.out.println(palavra));
+    }
+}
